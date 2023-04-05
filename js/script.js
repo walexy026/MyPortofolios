117ca24a9cea3f1f54c57e577bbb9df43106a01b
walexy026 <jao.olaniyi@gmail.com>
2023-04-05 21:13:09 +0100

    js updated

--- js/script.js
+++ js/script.js
@@ -2,3 +2,32 @@ const switcherToggler = document.querySelector(".style-toggle");
 const dayNight = document.querySelector(".day-night");
 const nav = document.querySelectorAll(".nav li a");
 const section = document.querySelectorAll(".section");
+
+const asideBtn = () => {
+  left.classList.toggle("open");
+  navBarIcon.classList.toggle("open");
+  section.forEach((set) => {
+    set.classList.toggle("open");
+  });
+};
+
+navBarIcon.addEventListener("click", asideBtn);
+
+icons.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+});
+
+let typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Designer",
+    "Software Engineer",
+    "Frontend Developer",
+    "Website Inventor",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
