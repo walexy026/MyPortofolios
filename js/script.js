7b048726cf84e271351bb178121ba304a576d3f9
walexy026 <jao.olaniyi@gmail.com>
2023-04-05 19:45:39 +0100

    started the js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const switcherToggler = document.querySelector(".style-toggle");
+const dayNight = document.querySelector(".day-night");
+const nav = document.querySelectorAll(".nav li a");
+const section = document.querySelectorAll(".section");
