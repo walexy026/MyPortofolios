f12c28bb04886f25f0cd8c30cab98e0a9a59b1b0
walexy026 <jao.olaniyi@gmail.com>
2023-04-05 23:51:23 +0100

    continued...tt

--- js/script.js
+++ js/script.js
@@ -31,3 +31,49 @@ let typed = new Typed(".typing", {
   BackSpeed: 60,
   loop: true,
 });
+
+switcherToggler.addEventListener("click", () =>
+  switchContent.classList.toggle("open")
+);
+
+const closeSwitch = () => {
+  if (switchContent.classList.contains("open")) {
+    switchContent.classList.remove("open");
+  }
+};
+window.addEventListener("scroll", closeSwitch);
+
+const darkMood = () => {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  } else {
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+};
+window.addEventListener("load", darkMood);
+
+const RemoveNav = () => {
+  nav.forEach((list) => {
+    list.classList.remove("active");
+  });
+};
+
+const showSection = (element) => {
+  section.forEach((set) => {
+    set.classList.remove("active");
+  });
+  const target = element.getAttribute("href").split("#")[1];
+  document.querySelector("#" + target).classList.add("active");
+  // document.querySelector("#" + target).classList.add("backSect");
+};
+
+nav.forEach((list) => {
+  list.addEventListener("click", () => {
+    RemoveNav();
+    showSection(list);
+    if (window.innerWidth < 1200) {
+      asideBtn();
+    }
+    list.classList.add("active");
+  });
+});
